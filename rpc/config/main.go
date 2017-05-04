35711a872e3643a2a9743d9a50482cbef0340edf
alanrong <alanrong@gmail.com>
2017-05-04 02:36:36 -0700

    add fields for check_update

--- rpc/config/main.go
+++ rpc/config/main.go
@@ -24,7 +24,7 @@ var w *nsq.Producer
 func (s *server) CheckUpdate(ctx context.Context, in *common.CommRequest) (*config.VersionReply, error) {
 	log.Printf("CheckUpdate request:%v", in)
 	util.PubRPCRequest(w, "config", "CheckUpdate")
-	version, desc := checkUpdate(db, in.Head.Term, in.Head.Version)
+	version, desc, title, subtitle, downurl := checkUpdate(db, in.Head.Term, in.Head.Version)
 	if version == "" {
 		return &config.VersionReply{
 			Head: &common.Head{Retcode: common.ErrCode_CHECK_UPDATE,
@@ -33,7 +33,8 @@ func (s *server) CheckUpdate(ctx context.Context, in *common.CommRequest) (*conf
 	util.PubRPCSuccRsp(w, "config", "CheckUpate")
 	return &config.VersionReply{
 		Head:    &common.Head{Retcode: 0, Uid: in.Head.Uid},
-		Version: version, Desc: desc}, nil
+		Version: version, Desc: desc, Title: title, Subtitle: subtitle,
+		Downurl: downurl}, nil
 }
 
 func main() {
--- rpc/config/version.go
+++ rpc/config/version.go
@@ -5,9 +5,9 @@ import (
 	"log"
 )
 
-func checkUpdate(db *sql.DB, term, version int64) (vname, desc string) {
-	err := db.QueryRow("SELECT vname, description FROM app_version WHERE term = ? AND version > ? ORDER BY  version DESC LIMIT 1",
-		term, version).Scan(&vname, &desc)
+func checkUpdate(db *sql.DB, term, version int64) (vname, desc, title, subtitle, downurl string) {
+	err := db.QueryRow("SELECT vname, description, title, subtitle, downurl FROM app_version WHERE term = ? AND version > ? ORDER BY  version DESC LIMIT 1",
+		term, version).Scan(&vname, &desc, &title, &subtitle, &downurl)
 	if err != nil {
 		log.Printf("checkUpdate query failed:%v", err)
 	}
