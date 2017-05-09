fc32ce7a4fc0a46c6d89c997140e36b21d4630d0
yuntifree <yuntifree@qq.com>
2017-05-09 11:50:47 +0800

    fix fetchShare

--- rpc/share/share.go
+++ rpc/share/share.go
@@ -457,9 +457,7 @@ func getRecommendShares(db *sql.DB, uid, tag int64) (infos []*share.ShareDetail,
 
 func fetchShares(db *sql.DB, seq, num, rtype int64) []*share.ShareInfo {
 	query := "SELECT s.id, s.uid, u.headurl, u.nickname, m.img, m.views, m.title, m.abstract FROM shares s, users u, media m WHERE s.uid = u.uid AND s.mid = m.id AND s.deleted = 0"
-	if rtype != 0 {
-		query += fmt.Sprintf(" AND review = %d", rtype)
-	}
+	query += fmt.Sprintf(" AND review = %d", rtype)
 	query += fmt.Sprintf(" ORDER BY s.id DESC LIMIT %d, %d", seq, num)
 	rows, err := db.Query(query)
 	if err != nil {
