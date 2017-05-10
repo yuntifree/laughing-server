65ae812e920e0aad680ec3a2dfcb9c544ffc2b8f
yuntifree <yuntifree@qq.com>
2017-05-10 15:11:17 +0800

    fix cdn for origin=0

--- rpc/share/share.go
+++ rpc/share/share.go
@@ -32,6 +32,9 @@ func addMediaTags(db *sql.DB, mid int64, tags []int64) {
 }
 
 func addShare(db *sql.DB, in *share.ShareRequest) (id int64, err error) {
+	if in.Origin == 0 && in.Dst != "" {
+		in.Cdn = in.Dst
+	}
 	res, err := db.Exec("INSERT INTO media(uid, title, img, dst, abstract, origin, width, height, thumbnail, src, cdn, ctime) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())",
 		in.Head.Uid, in.Title, in.Img, in.Dst, in.Desc, in.Origin,
 		in.Width, in.Height, in.Thumbnail, in.Src, in.Cdn)
