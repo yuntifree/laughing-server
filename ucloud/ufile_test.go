017f05dbb7bbae3128157d46fda8022aeca89776
alanrong <alanrong@gmail.com>
2017-05-14 23:01:43 -0700

    add test for GenHeadurl

--- ucloud/ufile_test.go
+++ ucloud/ufile_test.go
@@ -15,3 +15,16 @@ func Test_PutFile(t *testing.T) {
 		t.Errorf("PutFile failed")
 	}
 }
+
+func Test_GenHeadurl(t *testing.T) {
+	m := map[string]string{
+		"": "",
+		"http://img.yunxingzh.com/e5fc5ff5-302b-43fc-b2ef-26b5652a6479.jpg": "http://img.yunxingzh.com/e5fc5ff5-302b-43fc-b2ef-26b5652a6479.jpg",
+		"e5fc5ff5-302b-43fc-b2ef-26b5652a6479.jpg":                          cdn + "/e5fc5ff5-302b-43fc-b2ef-26b5652a6479.jpg",
+	}
+	for k, v := range m {
+		if GenHeadurl(k) != v {
+			t.Errorf("GenHeadurl failed:%s-%s:%s", k, v, GenHeadurl(k))
+		}
+	}
+}
