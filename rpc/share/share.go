be8d1d262907628beacd0259ce89181c03eaadf9
alanrong <alanrong@gmail.com>
2017-05-18 20:53:57 +0800

    add smile for fetchShares

--- rpc/share/share.go
+++ rpc/share/share.go
@@ -460,7 +460,7 @@ func getRecommendShares(db *sql.DB, uid, tag, sid int64) (infos []*share.ShareDe
 }
 
 func fetchShares(db *sql.DB, seq, num, rtype int64) []*share.ShareInfo {
-	query := "SELECT s.id, s.uid, u.headurl, u.nickname, m.img, m.views, m.title, m.abstract, m.id FROM shares s, users u, media m WHERE s.uid = u.uid AND s.mid = m.id AND s.deleted = 0 AND s.sid = 0 "
+	query := "SELECT s.id, s.uid, u.headurl, u.nickname, m.img, m.views, m.title, m.abstract, m.id, m.smile FROM shares s, users u, media m WHERE s.uid = u.uid AND s.mid = m.id AND s.deleted = 0 AND s.sid = 0 "
 	query += fmt.Sprintf(" AND review = %d", rtype)
 	query += fmt.Sprintf(" ORDER BY s.id DESC LIMIT %d, %d", seq, num)
 	rows, err := db.Query(query)
@@ -474,7 +474,7 @@ func fetchShares(db *sql.DB, seq, num, rtype int64) []*share.ShareInfo {
 		var info share.ShareInfo
 		var mid int64
 		err = rows.Scan(&info.Id, &info.Uid, &info.Headurl, &info.Nickname,
-			&info.Img, &info.Views, &info.Title, &info.Desc, &mid)
+			&info.Img, &info.Views, &info.Title, &info.Desc, &mid, &info.Smile)
 		if err != nil {
 			log.Printf("fetchShares scan failed:%v", err)
 			continue
@@ -488,7 +488,7 @@ func fetchShares(db *sql.DB, seq, num, rtype int64) []*share.ShareInfo {
 }
 
 func searchShares(db *sql.DB, sid int64) []*share.ShareInfo {
-	query := "SELECT s.id, s.uid, u.headurl, u.nickname, m.img, m.views, m.title, m.abstract, m.id FROM shares s, users u, media m WHERE s.uid = u.uid AND s.mid = m.id AND s.deleted = 0 "
+	query := "SELECT s.id, s.uid, u.headurl, u.nickname, m.img, m.views, m.title, m.abstract, m.id, m.smile FROM shares s, users u, media m WHERE s.uid = u.uid AND s.mid = m.id AND s.deleted = 0 "
 	query += fmt.Sprintf(" AND s.id = %d ORDER BY s.id DESC", sid)
 	rows, err := db.Query(query)
 	if err != nil {
@@ -501,7 +501,7 @@ func searchShares(db *sql.DB, sid int64) []*share.ShareInfo {
 		var info share.ShareInfo
 		var mid int64
 		err = rows.Scan(&info.Id, &info.Uid, &info.Headurl, &info.Nickname,
-			&info.Img, &info.Views, &info.Title, &info.Desc, &mid)
+			&info.Img, &info.Views, &info.Title, &info.Desc, &mid, &info.Smile)
 		if err != nil {
 			log.Printf("searchShares scan failed:%v", err)
 			continue
